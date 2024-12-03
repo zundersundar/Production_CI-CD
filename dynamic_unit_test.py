57922ef4fd465b78ead84c897983a22dccdc8cda
sundar <sundar.prasad@clockhash.com>
2024-12-03 19:48:38 +0530

    dynamic-test importing style changes

--- dynamic_unit_test.py
+++ dynamic_unit_test.py
@@ -1,5 +1,5 @@
 import unittest
-import heimdall_tools_test
+import heimdall_tools_test from unit_test
 #This is test of cicd 
 class DynamicTests(unittest.TestCase):
   #  def test_sqs_write(self):
