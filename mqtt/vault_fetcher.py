7cf4831e25cdcf05c43ec25ccb63105e105afa3f
zundersundar <sundar.prasad@clockhash.com>
2025-01-20 10:26:46 +0530

    123

--- mqtt/vault_fetcher.py
+++ mqtt/vault_fetcher.py
@@ -1,38 +1,33 @@
 import os
-from dotenv import set_key
 import hvac
+import logging
 
-# Path to the .env file
-ENV_FILE_PATH = "./.env"
+VAULT_ADDR = os.getenv("VAULT_ADDR")
+VAULT_USERNAME = os.getenv("VAULT_USERNAME")
+VAULT_PASSWORD = os.getenv("VAULT_PASSWORD")
+VAULT_USER_SECRETS_PATH = os.getenv("VAULT_USER_SECRETS_PATH")
+OUTPUT_FILE = "/app/.env"
 
-def write_to_env_file(key, value):
-    """Write a key-value pair to the .env file."""
-    set_key(ENV_FILE_PATH, key, value)
-
-def fetch_secrets_from_vault():
-    """Fetch secrets from Vault and write them to .env file."""
-    vault_addr = os.getenv("VAULT_ADDR")
-    vault_username = os.getenv("VAULT_USERNAME")
-    vault_password = os.getenv("VAULT_PASSWORD")
-    secrets_path = os.getenv("VAULT_USER_SECRETS_PATH")
-
-    # Authenticate with Vault
-    client = hvac.Client(url=vault_addr)
-    login_response = client.auth.userpass.login(username=vault_username, password=vault_password)
+try:
+    client = hvac.Client(url=VAULT_ADDR)
+    login_response = client.auth.userpass.login(
+        username=VAULT_USERNAME,
+        password=VAULT_PASSWORD
+    )
     if not client.is_authenticated():
-        raise Exception("Vault authentication failed!")
+        raise Exception("Failed to authenticate with Vault")
 
-    # Fetch secrets
-    common_secrets = client.secrets.kv.v2.read_secret(path="commonsecrets", mount_point="towerwatchsecrets")['data']['data']
-    user_secrets = client.secrets.kv.v2.read_secret(path=secrets_path, mount_point="towerwatchsecrets")['data']['data']
+    secrets = client.secrets.kv.v2.read_secret(
+        path=VAULT_USER_SECRETS_PATH,
+        mount_point="towerwatchsecrets"
+    )
 
-    # Write secrets to .env
-    for key, value in common_secrets.items():
-        write_to_env_file(key, value)
-    for key, value in user_secrets.items():
-        write_to_env_file(key, value)
+    with open(OUTPUT_FILE, "w") as env_file:
+        for key, value in secrets['data']['data'].items():
+            env_file.write(f"{key}={value}\n")
 
-    print("Secrets fetched and written to .env successfully.")
+    logging.info("Secrets successfully written to %s", OUTPUT_FILE)
 
-if __name__ == "__main__":
-    fetch_secrets_from_vault()
+except Exception as e:
+    logging.error("Error fetching or writing secrets: %s", e)
+    raise
